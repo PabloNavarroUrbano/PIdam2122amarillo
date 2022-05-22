407d342128b44bde98134ba61cf272e8623bca18
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-05-22 19:26:43 +0200

    cambios en la pagina web

--- DOC/MatchBook/script.js
+++
@@ -1,6 +0,0 @@
-const menuIconButton = document.querySelector("[data-menu-icon-btn]")
-const sidebar = document.querySelector("[data-sidebar]")
-
-menuIconButton.addEventListener("click", () => {
-  sidebar.classList.toggle("open")
-})
